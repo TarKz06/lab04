564ad996ef71a325cbc1d84339997a8c36d76b66
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-21 15:15:49 +0700

    1.1-1.3

---
+++ src/views/EventDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>{{ event.title }}</h1>
+        <p>{{ event.time}} on {{ event.date }} @ {{ event.location}}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            event : null
+        }
+    },
+    created(){
+        
+    }
+}
+</script>
