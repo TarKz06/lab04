59c9b89cfc4d97ca0d78a1f1b2c57b694cf1fd01
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-21 15:58:59 +0700

    1.10 path description

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -2,19 +2,20 @@
   <div v-if="event">
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
   </div>
 </template>
 
 <script>
+import EventService from '../services/EventService'
 export default {
+  props: ['id'],
   data() {
     return {
-      event: null,
-      id: 123
+      event: null
     }
   },
   created() {
-    // eslint-disable-next-line no-undef
     EventService.getEvent(this.id)
       .then((response) => {
         this.event = response.data
