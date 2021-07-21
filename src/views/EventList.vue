bfab8b51f8f9a2ba26428a3c153349a93870c80d
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-21 16:55:48 +0700

    2.6 add watchEffect

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -22,6 +22,7 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
 // import axios from 'axios'
 export default {
   name: 'EventList',
@@ -40,13 +41,15 @@ export default {
     }
   },
   created() {
-    EventService.getEvents(2, this.page)
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
   }
 }
 </script>
